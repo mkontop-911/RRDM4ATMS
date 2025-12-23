50e07d2829e73008385506936bd29eebf0004def
marios.kontopyrgos <marios.kontopyrgos@outlook.com>
2025-12-23 17:19:30 +0200

    Json File

--- RRDMJournalsTransfer/RRDMJournalsTransfer/RRDMJournalsTransferClass.cs
+++ RRDMJournalsTransfer/RRDMJournalsTransfer/RRDMJournalsTransferClass.cs
@@ -15,7 +15,7 @@ namespace TWF_ProcessFiles
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(System.IO.Directory.GetCurrentDirectory())
-                .AddJsonFile("JT_settings.json", optional: true, reloadOnChange: true);
+                .AddJsonFile("RRDMJournalsTransfer_appsettings.json", optional: true, reloadOnChange: true);
 
             _configuration = builder.Build();
         }
