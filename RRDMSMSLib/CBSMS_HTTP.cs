c0fe2320ee04902b0818044971b132daeaec180e
marios.kontopyrgos <marios.kontopyrgos@outlook.com>
2025-12-26 12:03:26 +0200

    RRDMSMSLib - convert to .net Core

--- RRDMSMSLib/CBSMS_HTTP.cs
+++ RRDMSMSLib/CBSMS_HTTP.cs
@@ -1,6 +1,6 @@
 using System;
-using System.Net;
-using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace bulksms_dotnetlib
 {
@@ -19,36 +19,26 @@ namespace bulksms_dotnetlib
          * for more info on using this with the BulkSMS API - contact info@bulksms.co.uk
          */
 
-        public string HTTPSend(string sHTTPInputData)
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<string> HTTPSendAsync(string sHTTPInputData)
         {
-
-            string sResult;                         //-- the result to be returned from this function
-            HttpWebResponse httpResponse = null;    //-- the response buffer used after the Request (HTTP GET) operation
-
+            string sResult;
 
             try
             {
-
                 //-- create the HTTP GET request
-                HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(sHTTPInputData);
-
-                //-- now get the response
-                httpResponse = (HttpWebResponse)(httpReq.GetResponse());
-                StreamReader input = new StreamReader(httpResponse.GetResponseStream());
-                sResult = input.ReadToEnd();
+                HttpResponseMessage response = await _httpClient.GetAsync(sHTTPInputData);
+                
+                // Ensure we throw on error to catch it below and return the error message formatted as expected
+                response.EnsureSuccessStatusCode();
+                
+                sResult = await response.Content.ReadAsStringAsync();
             }
-
             catch (Exception e)
             {
                 //-- something went wrong. Return result.
                 sResult = "HTTP Error: " + e.Message;
-            }
-
-            finally
-            {
-                //-- clean up
-                if (httpResponse != null)
-                    httpResponse.Close();
             }
 
             //-- return result
---
+++ RRDMSMSLibTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using bulksms_dotnetlib;
+
+Console.WriteLine("Testing RRDMSMSLib...");
+
+var smsClient = new CBSMS_Http();
+string testUrl = "https://www.example.com";
+
+Console.WriteLine($"Sending request to: {testUrl}");
+
+try
+{
+    string result = await smsClient.HTTPSendAsync(testUrl);
+    Console.WriteLine("Response received (first 100 chars):");
+    Console.WriteLine(result.Substring(0, Math.Min(result.Length, 100)));
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
+
+Console.WriteLine("Test completed.");
