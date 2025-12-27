6ff87fdad8d983a436b8131a52831a5dd40b0898
mkontop-911 <mkontop@gmail.com>
2025-12-27 13:25:41 +0200

    .

--- AutoOperationsTest/Program.cs
+++ AutoOperationsTest/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoOperations;
+using Microsoft.Extensions.Configuration;
 
 namespace AutoOperationsTest
 {
@@ -11,11 +12,18 @@ namespace AutoOperationsTest
     {
         static void Main(string[] args)
         {
-            // AutoOperation aut = new AutoOperation();
+            // Build configuration from appsettings.json
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("AutoOperations_appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
 
-            RRDM_Auto_Load_Match aut = new RRDM_Auto_Load_Match(); 
+            // Inject configuration into AutoOperations library
+            RRDM_Auto_Load_Match.ConfigureConfiguration(configuration);
 
-            aut.MasterAuto("Controller"); 
+            // Execute the auto operation
+            RRDM_Auto_Load_Match aut = new RRDM_Auto_Load_Match();
+            aut.MasterAuto("Controller");
         }
     }
 }
